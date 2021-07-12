ed5f810892f2affabc24a91a3037bd0d5f934127
Pedro <puedroreis@gmail.com>
2021-07-12 17:21:50 -0300

    deploy to  prod

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,61 @@
 <template>
   <div class="hello">
-
-   ip: {{ip}}
-
-
+    Acessos Totais: {{acessWithoutFilter }}
+    <br/>
+    Acessos Por IP: {{ acessWithFilter }}
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
   name: "HelloWorld",
   props: {
     msg: String,
-    ip: String
   },
+  data: () => ({
+    acessWithoutFilter: "",
+    acessWithFilter: "",
+    disgraca: {
+      ip: "",
+      date: "",
+    },
+  }),
 
-  async created(){
+  async created() {
     await this.getIPFromAmazon();
+    await  this.postIp();
+    await this.countIpsWithoutFilter();
+    await this.countIpsWithtFilter()
   },
 
   methods: {
     async getIPFromAmazon() {
       let result = await axios.get("https://api.db-ip.com/v2/free/self");
+      this.disgraca.ip = result.data.ipAddress;
+    },
 
-      console.log(result.data.ipAddress);
-      this.ip=result.data.ipAddress;
+    async postIp() {
+      await axios.post(
+        "https://ssacapsapi.azurewebsites.net/api/AcessIP",
+        this.disgraca
+      );
+    },
+
+    async countIpsWithoutFilter() {
+      let result = await axios.get(
+        "https://ssacapsapi.azurewebsites.net/api/AcessIP"
+      );
+      this.acessWithoutFilter = result.data.length;
+    },
+
+    async countIpsWithtFilter() {
+      let result = await axios.get(
+        "https://ssacapsapi.azurewebsites.net/api/AcessIP/GetByGroup"
+      );
+      this.acessWithFilter = result.data.length;
     },
   },
-
-
-
 };
 </script>
 
